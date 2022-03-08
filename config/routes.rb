8e695555e504a016add65b6dba006959b160e49f
sitarane <julien@larevolution.re>
2022-03-08 20:27:21 +0100

    add sessions routes, point require_logged_in to it

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "pages#home"
   resource :users, except: :index
+  resource :sessions, only: [:new, :create, :destroy]
 end
