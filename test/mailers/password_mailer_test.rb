3d01e5382954fcc79047d1539955bdf21fed4c7b
sitarane <julien@larevolution.re>
2022-03-17 22:20:03 +0100

    add mailer test, fix default from

--- test/mailers/password_mailer_test.rb
+++ test/mailers/password_mailer_test.rb
@@ -5,8 +5,7 @@ class PasswordMailerTest < ActionMailer::TestCase
     mail = PasswordMailer.with(user: users(:one)).reset
     assert_equal 'Password reset', mail.subject
     assert_equal [users(:one).email], mail.to
-    #assert_equal ["from@example.com"], mail.from
+    assert_equal ["noreply@timeslo.ts"], mail.from
     assert_match users(:one).name, mail.body.encoded
   end
-
 end
--- test/mailers/welcome_mailer_test.rb
+++ test/mailers/welcome_mailer_test.rb
@@ -1,7 +1,11 @@
 require "test_helper"
 
 class WelcomeMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "welcome_email" do
+    mail = WelcomeMailer.with(user: users(:one)).welcome_email
+    assert_equal 'Welcome to Timeslots', mail.subject
+    assert_equal [users(:one).email], mail.to
+    assert_equal ["noreply@timeslo.ts"], mail.from
+    assert_match users(:one).name, mail.body.encoded
+  end
 end
