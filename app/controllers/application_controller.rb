45a98804c103fa515b267d50fe3ccf3bbdf5b6d9
sitarane <julien@larevolution.re>
2022-03-18 21:18:38 +0100

    add language switch

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,17 @@
 class ApplicationController < ActionController::Base
   before_action :set_current_user
+  around_action :switch_locale
   def set_current_user
     Current.user = User.find_by(id: session[:user_id]) if session[:user_id]
   end
   def require_user_logged_in!
     redirect_to new_sessions_path, alert: 'You must be signed in' if Current.user.nil?
+  end
+  def switch_locale(&action)
+    locale = params[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &action)
+  end
+  def default_url_options
+    { locale: I18n.locale }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,16 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "pages#home"
-  resource :users, except: :index
-  resource :sessions, only: [:new, :create, :destroy]
-  resource :password, only: [:edit]
-  resource :password_reset, only: [:new, :edit, :update, :create]
+  get '/:locale', to: 'pages#home'
+  root 'pages#home'
+  scope "/:locale" do
+    resource :users, except: :index
+    resource :sessions, only: [:new, :create, :destroy]
+    resource :password, only: [:edit]
+    resource :password_reset, only: [:new, :edit, :update, :create]
 
-  get 'signup', to: 'users#new'
-  get 'login', to: 'sessions#new'
-  get 'logout', to: 'sessions#destroy'
+    get 'signup', to: 'users#new'
+    get 'login', to: 'sessions#new'
+    get 'logout', to: 'sessions#destroy'
+  end
 end
