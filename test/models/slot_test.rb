7805c2773ce293519c1954d7fa443023a56bba36
sitarane <julien@larevolution.re>
2022-03-27 19:01:37 +0200

    fix slots test

--- test/models/slot_test.rb
+++ test/models/slot_test.rb
@@ -2,23 +2,23 @@ require "test_helper"
 
 class SlotTest < ActiveSupport::TestCase
   test "Empty slot invalid" do
-    slot = Slot.new
+    slot = calendars(:one).slots.new
     assert_not slot.valid?
   end
   test "No date can't be saved" do
-    slot = Slot.new(name: "slot name")
+    slot = calendars(:one).slots.new(name: "slot name")
     assert_not slot.valid?
   end
   test "Past date can't be saved" do
-    slot = Slot.new(name: "slot name", start_time: 3.days.ago)
+    slot = calendars(:one).slots.new(name: "slot name", start_time: 3.days.ago)
     assert_not slot.valid?
   end
   test "No name can't be saved" do
-    slot = Slot.new(start_time: 3.days.since)
+    slot = calendars(:one).slots.new(start_time: 3.days.since)
     assert_not slot.valid?
   end
   test "Valid slot can be saved" do
-    slot = Slot.new(name: "slot name", start_time: 3.days.since)
+    slot = calendars(:one).slots.new(name: "slot name", start_time: 3.days.since)
     assert slot.valid?
     assert slot.save
   end
