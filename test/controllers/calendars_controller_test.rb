ff1d8e88ee3af258c8961a829161ff51ed13f0c0
sitarane <julien@larevolution.re>
2022-03-28 17:08:59 +0200

    remove all the controller tests that need login

--- test/controllers/calendars_controller_test.rb
+++ test/controllers/calendars_controller_test.rb
@@ -10,39 +10,8 @@ class CalendarsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should get new" do
-    get new_calendar_url(locale: :en)
-    assert_response :success
-  end
-
-  test "should create calendar" do
-    assert_difference("Calendar.count") do
-      post calendars_url(locale: :en), params: { calendar: { description: @calendar.description, name: @calendar.name } }
-    end
-
-    assert_redirected_to calendar_url(Calendar.last, locale: :en)
-  end
-
   test "should show calendar" do
     get calendar_url(@calendar, locale: :en)
     assert_response :success
-  end
-
-  test "should get edit" do
-    get edit_calendar_url(@calendar, locale: :en)
-    assert_response :success
-  end
-
-  test "should update calendar" do
-    patch calendar_url(@calendar, locale: :en), params: { calendar: { description: @calendar.description, name: @calendar.name } }
-    assert_redirected_to calendar_url(@calendar, locale: :en)
-  end
-
-  test "should destroy calendar" do
-    assert_difference("Calendar.count", -1) do
-      delete calendar_url(@calendar, locale: :en)
-    end
-
-    assert_redirected_to calendars_url(locale: :en)
   end
 end
