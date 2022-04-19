144a8ce58c855fc44ae59bbfc511c2bed26322d0
sitarane <julien@larevolution.re>
2022-04-19 18:34:43 +0200

    New function: Add calendar editor

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -45,8 +45,11 @@ class CalendarsController < ApplicationController
   # PATCH/PUT /calendars/1 or /calendars/1.json
   def update
     authorize @calendar
+    add_editors if calendar_params[:users]
+    updated_params = calendar_params
+    updated_params.delete(:users)
     respond_to do |format|
-      if @calendar.update(calendar_params)
+      if @calendar.update(updated_params)
         format.html { redirect_to calendar_url(@calendar), notice: "Calendar was successfully updated." }
         format.json { render :show, status: :ok, location: @calendar }
       else
@@ -68,6 +71,21 @@ class CalendarsController < ApplicationController
   end
 
   private
+
+  def add_editors
+    if calendar_params[:users]
+      emails = calendar_params[:users]
+
+      # split at spaces and commas
+      list_of_emails = emails.split(/[,\s]+/)
+
+      list_of_users = Array.new
+      list_of_emails.each do |email|
+        list_of_users << User.find_by(email: email)
+      end
+      @calendar.users << list_of_users
+    end
+  end
 
   # Use callbacks to share common setup or constraints between actions.
   def set_calendar
