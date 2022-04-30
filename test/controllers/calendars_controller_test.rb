eb11e082f6963d03e8fa69e51e4e2af31b509c0b
sitarane <julien@larevolution.re>
2022-04-30 23:12:28 +0200

    add login to test + set up calendar controller test with login

--- test/controllers/calendars_controller_test.rb
+++ test/controllers/calendars_controller_test.rb
@@ -2,6 +2,14 @@ require "test_helper"
 
 class CalendarsControllerTest < ActionDispatch::IntegrationTest
   setup do
+    @mock_policy = Minitest::Mock.new
+    @mock_policy.expect :new?, true
+    @mock_policy.expect :create?, true
+    @mock_policy.expect :edit?, true
+    @mock_policy.expect :destroy?, true
+
+    @mock_user = Minitest::Mock.new
+    @mock_user.expect :user, users(:one)
     @calendar = calendars(:one)
   end
 
@@ -14,4 +22,33 @@ class CalendarsControllerTest < ActionDispatch::IntegrationTest
     get calendar_url(@calendar, locale: :en)
     assert_response :success
   end
+
+  test "Should show new form" do
+    log_in_as(users(:one))
+    get new_calendar_url(locale: :en)
+    assert_response :success
+  end
+
+  test "Should show edit form" do
+    log_in_as(users(:one))
+    get edit_calendar_url(@calendar, locale: :en)
+    assert_response :success
+  end
+
+  test "Should destroy calendar" do
+    log_in_as(users(:one))
+    delete calendar_url(@calendar, locale: :en)
+    assert_redirected_to calendars_url
+  end
+
+  test "Should create calendar" do
+    log_in_as(users(:one))
+    post calendars_url(locale: :en),
+    params: { calendar: {
+      name: "a calendar",
+      description: "bla bla bla description"
+    }}
+    assert_redirected_to calendar_url(Calendar.last)
+  end
+
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,7 @@
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
+require 'minitest/autorun'
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
@@ -9,7 +10,12 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+  # Setup capybara to work with the browser container
   Capybara.server_host = "0.0.0.0"
   Capybara.app_host = "http://#{ENV.fetch("HOSTNAME")}:#{Capybara.server_port}"
+
+  # Add more helper methods to be used by all tests here...
+  def log_in_as(user, password: 'password')
+      post sessions_path, params: { email: user.email, password: password}
+  end
 end
