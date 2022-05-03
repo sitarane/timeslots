3fd09c117b8e130b2ca29ff5b57c29a9f601032b
sitarane <julien@larevolution.re>
2022-05-03 17:43:15 +0200

    add calendar system test

--- test/system/calendars_test.rb
+++ test/system/calendars_test.rb
@@ -2,54 +2,16 @@ require "application_system_test_case"
 
 class CalendarsTest < ApplicationSystemTestCase
   setup do
-    @mock_user = Minitest::Mock.new
-    @mock_user.expect :user, users(:one)
-    #Current.user = users(:one)
-
-    @mock_policy = Minitest::Mock.new
-    @mock_policy.expect :new?, true
-    #@mock_policy.expect :create?, true
-    @calendar = calendars(:one)
+    @user = users(:one)
+    login(@user)
   end
 
-  test "visiting the index" do
+  test "Create a calendar" do
     visit calendars_url(locale: :en)
-    assert_selector "h1", text: "Calendars"
-  end
+    click_on "New calendar"
+    fill_in "Name", with: "My brand new calendar"
+    click_on "Create Calendar"
 
-  test "should create calendar" do
-    CalendarPolicy.stub :new, @mock_policy do
-      visit calendars_url(locale: :en)
-      click_on "New calendar" # Dummy. You aren't logged in.
-      debugger
-      fill_in "Description", with: @calendar.description
-      fill_in "Name", with: @calendar.name
-      fill_in "Avance Warning", with: @calendar.advance_warning
-      click_on "Create Calendar"
-
-      assert_text "Calendar was successfully created"
-      click_on "Back"
-    end
-    @mock_policy.verify
-  end
-
-  test "should update Calendar" do
-    visit calendar_url(@calendar, locale: :en)
-    click_on "Edit this calendar", match: :first
-
-    fill_in "Description", with: @calendar.description
-    fill_in "Name", with: @calendar.name
-    click_on "Update Calendar"
-
-    assert_text "Calendar was successfully updated"
-    click_on "Back"
-  end
-
-  test "should destroy Calendar" do
-    log_in_as(users(:one))
-    visit calendar_url(@calendar, locale: :en)
-    click_on "Destroy this calendar", match: :first
-
-    assert_text "Calendar was successfully destroyed"
+    assert_text "Calendar was successfully created"
   end
 end
