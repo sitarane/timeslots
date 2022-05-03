60e63ce882281729beb36a7b7e6758e4222d12a5
sitarane <julien@larevolution.re>
2022-05-03 21:46:15 +0200

    improve layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def logged_in?
     Current.user.blank?
   end
+  def page_title
+    return @page_title if @page_title.present?
+    return t(:app_name) if current_page?(root_path)
+    return controller_name.capitalize
+  end
+  def page_subtitle
+    t(:slogan) if current_page?(root_path)
+  end
 end
