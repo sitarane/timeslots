642cbbf8669e0e30f0f4436fb653f8223a4fe0ee
sitarane <julien@larevolution.re>
2022-05-03 17:31:10 +0200

    add user system test

--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -1,9 +1,14 @@
 require "application_system_test_case"
 
 class UsersTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit users_url
-  #
-  #   assert_selector "h1", text: "Users"
-  # end
+  test 'create new user' do
+    visit new_users_url
+    fill_in 'Name', with: 'Alberto'
+    fill_in 'Email', with: 'alberto@mail.com'
+    fill_in 'Password', with: 'superman69'
+    fill_in 'Password confirmation', with: 'superman69'
+    click_on "signup"
+
+    assert_text 'Successfully created account'
+  end
 end
