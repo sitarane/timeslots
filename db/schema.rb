3b00637e3d5753c1608110fd374f34991917a82b
sitarane <julien@larevolution.re>
2022-05-05 22:31:59 +0200

    add timezone to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_12_095954) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_05_201835) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -64,6 +64,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_12_095954) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "timezone"
   end
 
   add_foreign_key "bookings", "slots"
