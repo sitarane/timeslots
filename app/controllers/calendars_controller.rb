10cf99eba744a4114b5cf2ec74b19dbe76ee0690
sitarane <julien@larevolution.re>
2022-05-11 20:12:48 +0200

    tweak some controller

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -42,6 +42,7 @@ class CalendarsController < ApplicationController
   def update
     authorize @calendar
     add_editors if calendar_params[:users]
+    ## This looks ripe for refactor...
     updated_params = calendar_params
     updated_params.delete(:users)
     if @calendar.update(updated_params)
@@ -62,18 +63,16 @@ class CalendarsController < ApplicationController
   private
 
   def add_editors
-    if calendar_params[:users]
-      emails = calendar_params[:users]
+    emails = calendar_params[:users]
 
-      # split at spaces and commas
-      list_of_emails = emails.split(/[,\s]+/)
+    # split at spaces and commas
+    list_of_emails = emails.split(/[,\s]+/)
 
-      list_of_users = Array.new
-      list_of_emails.each do |email|
-        list_of_users << User.find_by(email: email)
-      end
-      @calendar.users << list_of_users
+    list_of_users = Array.new
+    list_of_emails.each do |email|
+      list_of_users << User.find_by(email: email)
     end
+    @calendar.users << list_of_users
   end
 
   # Use callbacks to share common setup or constraints between actions.
