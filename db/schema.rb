94153f2ef61e1540ec924774681cc2dcf94be3c9
sitarane <julien@larevolution.re>
2022-06-19 18:18:22 +0200

    add Public field to Calendars

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_11_084449) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_19_161452) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -39,6 +39,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_11_084449) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "advance_warning", default: 0, null: false
+    t.boolean "public", default: true
   end
 
   create_table "invitations", force: :cascade do |t|
