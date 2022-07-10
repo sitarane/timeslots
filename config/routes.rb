9c5c369dde6f09c2bbd4d758bd35f1e0ebffd5b8
sitarane <julien@larevolution.re>
2022-07-10 20:33:10 +0200

    add controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { sessions: 'users/sessions' }
   resources :bookings, only: [:create, :update]
   get '/:locale', to: 'pages#home'
   root 'pages#home'
