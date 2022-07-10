06bb8774472a9e389feb2ffd4156433edfde05ee
sitarane <julien@larevolution.re>
2022-07-10 18:55:30 +0200

    remove password digest from users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_10_164033) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_10_165105) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -65,7 +65,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_10_164033) do
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", null: false
-    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "timezone"
