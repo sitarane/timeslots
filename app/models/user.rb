ef6fef9ca2b7fcb012ce6b58c063f64e0fdf84de
sitarane <julien@larevolution.re>
2022-07-10 18:58:32 +0200

    remove has_secure_password from User

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,9 +9,6 @@ class User < ApplicationRecord
   has_many :calendar_assignations, dependent: :destroy
   has_many :calendars, through: :calendar_assignations
 
-  # adds virtual attributes for authentication
-  has_secure_password
-
   validates :name, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'Invalid email' }
 end
